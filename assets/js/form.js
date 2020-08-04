374b25943a0df3809aeef311f60bd4a9feeba5f0
ProfesseurOrme <matth.guilloteau@gmail.com>
2020-08-04 16:48:41 +0200

    Ajout Figure : Finalisation du formulaire + correctifs concernant les upload. ToDo : Pouvoir persister la figure en BDD

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,38 +1,29 @@
+$(document).on('change', '.file-input-element', function () {
+
+    let fileName = '';
+    for(let i = 0; i < $(this).get(0).files.length; i++) {
+        (i >= 1) ? fileName += ' / ' +  $(this).get(0).files[i].name.replace(/^C:\\fakepath\\/i, '') : fileName += $(this).get(0).files[i].name.replace(/^C:\\fakepath\\/i, '');
+    }
+    $(this).parent('.form-file').find('.input-file-value').text(fileName);
+})
+
 let collectionHolderPicture, collectionHolderVideo;
-
 let addTagButtonVideo = $('.add_tag_link_video');
-
-//et newLinkPicture = $('<div class="add-picture-block"></div>');
 let newLinkVideo = $('<div class="add-video-block"></div>');
 
 $(document).ready(function() {
-    //collectionHolderPicture = $('div.add-picture');
+
     collectionHolderVideo = $('div.add-video');
-
-    //collectionHolderPicture.append(newLinkPicture);
     collectionHolderVideo.append(newLinkVideo);
-
-    //collectionHolderPicture.data('index', collectionHolderPicture.find('input').length);
     collectionHolderVideo.data('index', collectionHolderVideo.find('input').length);
-
-    /*if (collectionHolderPicture.find(':input').length < 1) {
-        addTagForm(collectionHolderPicture,newLinkPicture);
-    } */
 
     if(collectionHolderVideo.find(':input').length < 1){
         addTagForm(collectionHolderVideo,newLinkVideo);
     }
 
-    /*addTagButtonPicture.on('click', function() {
-
-        addTagForm(collectionHolderPicture, newLinkPicture);
-    });*/
-
     addTagButtonVideo.on('click', function() {
-
         addTagForm(collectionHolderVideo, newLinkVideo);
     });
-
 });
 
 function addTagForm(collectionHolder, newLink) {
@@ -42,13 +33,9 @@ function addTagForm(collectionHolder, newLink) {
     let newForm = prototype;
 
     newForm = newForm.replace(/__name__/g, index);
+
     collectionHolder.data('index', index + 1);
-    /*let newFormLine;
-    if(newLink.hasClass('add-picture-block')) {
-        newFormLine = $('<div class="add-picture-block"></div>').append(newForm);
-    } else {
-        newFormLine = $('<div class="add-video-block"></div>').append(newForm);
-    } */
+
     let newFormLine = $('<div class="add-video-block"></div>').append(newForm);
 
     newLink.before(newFormLine);
